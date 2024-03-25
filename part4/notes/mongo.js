beeb21ddb48b6079f328c56af67e4b2a4c26af2a
Miguel García <miguel.garglez@gmail.com>
2024-03-25 21:13:47 +0100

    Test environment config, left on executing API tests one by one

--- part4/notes/mongo.js
+++ part4/notes/mongo.js
@@ -8,7 +8,7 @@ if (process.argv.length < 3) {
 const password = process.argv[2]
 
 const url =
-  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/noteApp?retryWrites=true&w=majority`
+  `mongodb+srv://fullstackopen:${password}@clusterfso.ztr116a.mongodb.net/testNoteApp?retryWrites=true&w=majority&appName=ClusterFS0`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url).then(() => {
@@ -19,17 +19,17 @@ mongoose.connect(url).then(() => {
 
   const Note = mongoose.model('Note', noteSchema)
 
-  /*
-  const note = new Note({
-    content: 'HTML is x',
-    important: true,
+  
+  /*const note = new Note({
+    content: 'Another note to test API',
+    important: false,
   })
 
-  note.save().then(result => {
+  note.save().then(() => {
     console.log('note saved!')
     mongoose.connection.close()
-  })
-  */
+  })*/
+  
   Note.find({}).then(result => {
     result.forEach(note => {
       console.log(note)
