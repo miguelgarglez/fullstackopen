35d7dadabe85046a9ef7b48f6dbe26583bc6cb49
Miguel García <miguel.garglez@gmail.com>
2024-04-04 21:39:09 +0200

    exercises until 4.20 done

--- part4/blogs/controllers/blogs.js
+++ part4/blogs/controllers/blogs.js
@@ -2,6 +2,8 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 const logger = require('../utils/logger')
+const jwt = require('jsonwebtoken')
+const process = require('process')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -11,15 +13,18 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
-  const users = await User.find({})
-  const user = users[0]
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   logger.info('user', user)
   
   const blog = new Blog({
     title: body.title,
     author: body.author,
-    user: user.id,
+    user: user._id,
     url: body.url
     // likes will be 0 by default
   })
--- part4/blogs/utils/middleware.js
+++ part4/blogs/utils/middleware.js
@@ -19,13 +19,33 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({
+      error: 'invalid token'
+    })
+  } else if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({
+      error: 'token expired'
+    })
   }
 
   next(error)
 }
 
+const tokenExtractor = (request, response, next) => {
+  // código que extrae el token
+  const authorization = request.get('authorization') || ''
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+  } else {
+    request.token = null
+  }
+  next()
+}
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 }
