a9d36f0f150a0ff7e325f0467b5d5c120b788e85
Miguel García <miguel.garglez@gmail.com>
2024-04-11 23:40:41 +0200

    5.7 done

--- part5/blogs_fe/src/components/Blog.jsx
+++ part5/blogs_fe/src/components/Blog.jsx
@@ -1,7 +1,37 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [visibleDetails, setVisibleDetails] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const toggleVisibility = () => {
+    setVisibleDetails(!visibleDetails);
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} - {blog.author}{" "}
+        <button onClick={toggleVisibility}>
+          {visibleDetails ? "hide" : "view"}
+        </button>
+        <div style={{ display: visibleDetails ? "" : "none" }}>
+          <div>{blog.url}</div>
+          <div>
+            likes: {blog.likes} <button>like</button>
+          </div>
+          {blog.user ? <div>{blog.user.name}</div> : null}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
