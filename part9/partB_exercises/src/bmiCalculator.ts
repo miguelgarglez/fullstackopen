803e74d2a038615a0b94665c6ab6edeb02498dc6
Miguel García <miguel.garglez@gmail.com>
2024-06-15 13:53:41 +0200

    part9 exercises 9.1-9.3 done

---
+++ part9/partB_exercises/src/bmiCalculator.ts
@@ -0,0 +1,43 @@
+
+interface BmiValues {
+    height: number;
+    weight: number;
+}
+
+const parseArguments = (args: Array<string>): BmiValues => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments');
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            height: Number(args[2]),
+            weight: Number(args[3])
+        };
+    } else {
+        throw new Error('Provided values were not numbers!');
+    }
+}
+
+
+const calculateBmi = (height: number, weight: number): string => {
+
+    const bmi = weight / ((height / 100) ** 2);
+    
+    if (bmi < 18.5) {
+        return 'Underweight';
+    } else if (bmi >= 18.5 && bmi < 25) {
+        return 'Normal (healthy weight)';
+    } else if (bmi >= 25 && bmi < 30) {
+        return 'Overweight';
+    } else {
+        return 'Obese';
+    }
+}
+
+try {
+    const { height, weight } = parseArguments(process.argv);
+    console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+    if (error instanceof Error) {
+        console.log('Error:', error.message);
+    }
+}
---
+++ part9/partB_exercises/src/exerciseCalculator.ts
@@ -0,0 +1,57 @@
+interface ExerciseResult {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+interface ExerciseValues {
+    target: number;
+    dailyExerciseHours: Array<number>;
+}
+
+const parseExerciseArguments = (args: Array<string>): ExerciseValues => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    const target = Number(args[2]);
+    const dailyExerciseHours = args.slice(3).map(hours => Number(hours));
+    if (!isNaN(target) && dailyExerciseHours.every(hours => !isNaN(hours))) {
+        return {
+            target,
+            dailyExerciseHours
+        };
+    } else {
+        throw new Error('Provided values were not numbers!');
+    }
+}
+
+
+const calculateExercises = (dailyExerciseHours: Array<number>, target: number): ExerciseResult => {
+    const periodLength = dailyExerciseHours.length;
+    const trainingDays = dailyExerciseHours.filter(hours => hours > 0).length;
+    const average = dailyExerciseHours.reduce((a, b) => a + b) / periodLength;
+    const success = average >= target;
+    const rating = average >= target ? 3 : average >= target - 1 ? 2 : 1;
+    const ratingDescription = rating === 3 ? 'Great job!' : rating === 2 ? 'Not too bad but could be better' : 'You need to work harder';
+    
+    return {
+        periodLength,
+        trainingDays,
+        success,
+        rating,
+        ratingDescription,
+        target,
+        average
+    };
+}
+
+try {
+    const { target, dailyExerciseHours } = parseExerciseArguments(process.argv);
+    console.log(calculateExercises(dailyExerciseHours, target));
+} catch (error: unknown) {
+    if (error instanceof Error) {
+        console.log('Error:', error.message);
+    }
+}
